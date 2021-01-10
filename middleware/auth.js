009f0f10364227381f3496ef389c57f31ff8af5f
visotoniki9 <d.simon.ssx0@gmail.com>
2021-01-10 20:24:32 +0100

    jwt validation added

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  // Get token from the header
+  const token = req.header('x-auth-token');
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token found, authorization denid' });
+  }
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ msg: 'Token is not valid' });
+  }
+  return res.status(200).json({ msg: 'Valid token' });
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const auth = require('../../middleware/auth');
+
 // @route GET api/auth
 // @desc  Test route
 // @access Public
-router.get('/', (req, res) => res.send('auth route'));
+router.get('/', auth, (req, res) => res.send('auth route'));
 
 module.exports = router;
