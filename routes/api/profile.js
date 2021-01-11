aefff4d810f8f59124e2a1ee125edd3d5bf666a7
visotoniki9 <d.simon.ssx0@gmail.com>
2021-01-11 19:01:18 +0100

    added delete experiece api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -126,7 +126,7 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
-// @route PUT api/profile
+// @route PUT api/profile/experience
 // @desc update profile experience field
 // @access Private
 router.put(
@@ -156,4 +156,26 @@ router.put(
 
 );
 
+// @route DELETE api/profile/experience/:exp_id
+// @desc delete experience from profile experience field
+// @access Private
+router.delete(
+  '/experience/:exp_id',
+  auth,
+  async (req, res) => {
+    try {
+      const foundProfile = await Profile.findOne({ user: req.user.id });
+      const removeIndex = foundProfile.experience
+        .map((item) => item.id)
+        .indexOf(req.params.exp_id);
+      foundProfile.experience.splice(removeIndex, 1);
+      await foundProfile.save();
+      res.json(foundProfile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  },
+
+);
 module.exports = router;
